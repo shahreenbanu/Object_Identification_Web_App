b40c03e8c988580356c6eae7454aafb97be8482a
shahreenbanu <85785868+shahreenbanu@users.noreply.github.com>
2023-08-07 16:22:07 -0700

    Add files via upload

---
+++ windows.js
@@ -0,0 +1,30 @@
+Status = "";
+windows_image = "";
+
+function preload(){
+    windows_image = loadImage("OIG (1).jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(windows_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(windows_image,0,0,640,350);
+}
